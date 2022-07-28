bb11cd98232ad4abc9da6a3c51a9ba0c6aafb5fd
rutatiina <rutatiina@ruginem.org>
2022-07-28 10:46:55 +0300

    updates

--- src/Services/GoodsReturnedService.php
+++ src/Services/GoodsReturnedService.php
@@ -25,11 +25,10 @@ class GoodsReturnedService
     {
         return GoodsReturnedSetting::firstOrCreate([
             'tenant_id' => session('tenant_id'),
-            'document_name' => 'Goods Delivered Note',
+            'document_name' => 'Goods Returned Note',
             'document_type' => 'inventory',
-            //since this is not a financial accounting entry, there is no need for the double entry details
-            //'debit_financial_account_code' => 720100, //Cost of Sales
-            //'credit_financial_account_code' => 130500, //Other Inventory [value was 6 before changing to codes]
+            // 'debit_financial_account_code' => 130500, //Other Inventory
+            // 'credit_financial_account_code' => 66, //sales person inventory
         ]);
     }
 
