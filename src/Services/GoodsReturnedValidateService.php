5de19e4fc714c6e93c9206c90f6bccd80989c7a0
Gilbert Rutatiina <rutatiina@gmail.com>
2023-05-23 00:18:14 +0300

    Item balances

--- src/Services/GoodsReturnedValidateService.php
+++ src/Services/GoodsReturnedValidateService.php
@@ -92,7 +92,7 @@ class GoodsReturnedValidateService
                 'tenant_id' => $data['tenant_id'],
                 'created_by' => $data['created_by'],
                 'contact_id' => $item['contact_id'],
-                'item_id' => $item['item_id'],
+                'item_id' => optional($itemModel)->id, //$item['item_id'], use internal ID to verify data so that from here one the item_id value is LEGIT
                 'name' => $item['name'],
                 'description' => $item['description'],
                 'quantity' => $item['quantity'],
